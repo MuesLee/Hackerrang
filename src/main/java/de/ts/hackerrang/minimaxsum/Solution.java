694e5e628b0b2ba80e142100f940e3ab7b3ea7cd
MuesLee <stimo@gmx.net>
2020-08-04 12:03:00 +0200

    feat: MiniMaxSum puzzle solved.

---
+++ src/main/java/de/ts/hackerrang/minimaxsum/Solution.java
@@ -0,0 +1,40 @@
+package de.ts.hackerrang.minimaxsum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Complexity: O(n log(n))
+ */
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        Arrays.sort(arr);
+
+        long totalSum = Arrays.stream(arr).asLongStream().sum();
+        long maxSum = totalSum - arr[0];
+        long minSum = totalSum - arr[arr.length-1];
+
+        System.out.print(minSum + " " + maxSum);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
+
---
+++ src/test/java/de/ts/hackerrang/minimaxsum/SolutionTest.java
@@ -0,0 +1,57 @@
+package de.ts.hackerrang.minimaxsum;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.ArgumentsSources;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private PrintStream standardOutput;
+
+    private PrintStream testPrintStream;
+    private ByteArrayOutputStream testOutput;
+
+    private static Stream<Arguments> testArguments() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, "10 14"),
+                Arguments.of(new int[]{256741038, 623958417, 467905213, 714532089, 938071625}, "2063136757 2744467344")
+        );
+    }
+
+
+    @BeforeEach
+    void setup() {
+        standardOutput = System.out;
+        testOutput = new ByteArrayOutputStream();
+        testPrintStream = new PrintStream(testOutput);
+
+        System.setOut(testPrintStream);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testArguments")
+    void miniMaxSum(int[] givenNumbers, String expectedMinMax) {
+        String expectedMinMaxString = "2063136757 2744467344";
+
+        Solution.miniMaxSum(new int[]{256741038, 623958417, 467905213, 714532089, 938071625});
+        String actual = testOutput.toString();
+
+        Assertions.assertThat(actual).isEqualTo(expectedMinMaxString);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        testPrintStream.close();
+        System.setOut(standardOutput);
+    }
+}
