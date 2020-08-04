b33e414039cdbdab6b45fc45b8d2ae3fd7199564
MuesLee <stimo@gmx.net>
2020-08-04 11:22:39 +0200

    feat: staircase puzzle solved.

---
+++ src/main/java/de/ts/hackerrang/staircase/Solution.java
@@ -0,0 +1,40 @@
+package de.ts.hackerrang.staircase;
+
+import java.util.Scanner;
+
+/**
+ * Complexity: O(n)
+ */
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int maxStairLevel) {
+
+
+        for (int stairLevel = 0; stairLevel < maxStairLevel; stairLevel++) {
+            for (int j = 0; j < maxStairLevel - stairLevel - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int stairsAtLevel = 0; stairsAtLevel <= stairLevel; stairsAtLevel++) {
+                System.out.print("#");
+            }
+
+            boolean isLastLevel = stairLevel == maxStairLevel - 1;
+            if (!isLastLevel) {
+                System.out.println();
+            }
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
---
+++ src/test/java/de/ts/hackerrang/staircase/SolutionTest.java
@@ -0,0 +1,51 @@
+package de.ts.hackerrang.staircase;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class SolutionTest {
+
+
+    private PrintStream standardOutput;
+
+    private PrintStream testPrintStream;
+    private ByteArrayOutputStream testOutput;
+
+
+    @BeforeEach
+    void setup() {
+        standardOutput = System.out;
+        testOutput = new ByteArrayOutputStream();
+        testPrintStream = new PrintStream(testOutput);
+
+        System.setOut(testPrintStream);
+    }
+
+    @Test
+    void staircase() {
+        String expectedStaircase =
+                "     #" + System.lineSeparator() +
+                "    ##" + System.lineSeparator() +
+                "   ###" + System.lineSeparator() +
+                "  ####" + System.lineSeparator() +
+                " #####" + System.lineSeparator() +
+                "######";
+
+        Solution.staircase(6);
+        String actual = testOutput.toString();
+
+        Assertions.assertThat(actual).isEqualTo(expectedStaircase);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        testPrintStream.close();
+        System.setOut(standardOutput);
+    }
+}
